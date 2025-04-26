f4b1297b5440a4c9b09d8ffcfb33cac564fc075c
Paul Swingle <swingle8paul@gmail.com>
2025-04-26 16:15:45 -0700

    cli

--- src/main.cpp
+++ src/main.cpp
@@ -4,17 +4,23 @@
 #include "utils.h"
 #include "lexer/lexer.h"
 #include "ast/ast.h"
+#include "cli/cli.h"
 
 
-int main() {
-    std::string text = readFile("test.ax");
+int main(int argc, char* argv[]) {
+    CLIArgs args;
+    if (!args.parse(argc, argv)) {
+        return 1;
+    }
+
+    std::string text = readFile(args.inputFile);
     Lexer lexer(text);
 
     auto mainBlock = parseBlock(lexer, true);
     ModuleState module;
     if (mainBlock && mainBlock->codegen(module)) {
         std::cout << "successfully compiled!" << std::endl;
-        module.writeIR("out.bc");
+        module.writeIR(args.outputFile, !args.outputLL);
     } else {
         std::cout << "did not successfully compile :(";
     }

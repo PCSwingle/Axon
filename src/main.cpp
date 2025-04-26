4a525b5d85cf69724cae8155ea658bb71a0e221f
Paul Swingle <swingle8paul@gmail.com>
2025-04-26 15:46:25 -0700

    utils

--- src/main.cpp
+++ src/main.cpp
@@ -1,33 +1,10 @@
 #include <fstream>
 #include <iostream>
 
+#include "utils.h"
 #include "lexer/lexer.h"
 #include "ast/ast.h"
 
-std::string readStdin() {
-    std::cout << "code >" << std::endl;
-    std::string text;
-    std::string line;
-    while (getline(std::cin, line)) {
-        text += line + "\n";
-    }
-    return text;
-}
-
-std::string readFile(const std::string& filename) {
-    std::ifstream file(filename);
-    if (!file.is_open()) {
-        std::cerr << "Error opening file!" << std::endl;
-        return "";
-    }
-    std::string text;
-    std::string line;
-    while (getline(file, line)) {
-        text += line + "\n";
-    }
-    file.close();
-    return text;
-}
 
 int main() {
     std::string text = readFile("test.ax");
--- src/utils.h
+++ src/utils.h
@@ -1,8 +1,37 @@
 #pragma once
+#include <llvm/IR/Type.h>
+#include <fstream>
 
-inline std::string typeToString(llvm::Type* type) {
+using namespace llvm;
+
+inline std::string typeToString(Type* type) {
     std::string S;
-    llvm::raw_string_ostream OS(S);
+    raw_string_ostream OS(S);
     type->print(OS);
     return OS.str();
+}
+
+inline std::string readStdin() {
+    std::cout << "code >" << std::endl;
+    std::string text;
+    std::string line;
+    while (getline(std::cin, line)) {
+        text += line + "\n";
+    }
+    return text;
+}
+
+inline std::string readFile(const std::string& filename) {
+    std::ifstream file(filename);
+    if (!file.is_open()) {
+        std::cerr << "Error opening file!" << std::endl;
+        return "";
+    }
+    std::string text;
+    std::string line;
+    while (getline(file, line)) {
+        text += line + "\n";
+    }
+    file.close();
+    return text;
 }
